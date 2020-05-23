e3c3e2cb3b7ac0f12dc533fec72fe56200349624
topherPedersen <chrispedatx@gmail.com>
2020-05-22 21:59:04 -0500

    added container for flatlist

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   SafeAreaView,
   View,
   Text,
+  FlatList,
 } from 'react-native';
 
 // React-Native Paper
@@ -14,15 +15,28 @@ import { FAB } from 'react-native-paper';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      todo: [
+        {id: "f00", task: "foo"},
+        {id: "b@r", task: "bar"},
+        {id: "b@z", task: "baz"},
+      ],
+    };
   }
 
   render() {
+
     return(
       <PaperProvider>
         <SafeAreaView style={{flex: 1, backgroundColor: "white"}}>
 
           <View style={{position: 'absolute', top: "10%", backgroundColor: "white", width: "100%"}}>
             <Text style={{textAlign: 'center', fontSize: 20}}>Topher's TODO App</Text>
+          </View>
+
+          <View style={{alignSelf: 'center', marginTop: "40%", width: "100%", height: "100%", backgroundColor: "white"}}>
+
           </View>
 
           <View style={{position: 'absolute', bottom: "10%", backgroundColor: "white", width: "100%"}}>
