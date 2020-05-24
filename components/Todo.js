fb8309cbc206aced3f87a186a6fd0582ad954f0a
topherPedersen <chrispedatx@gmail.com>
2020-05-24 15:52:25 -0500

    added logic to trim todo item text if too long

--- components/Todo.js
+++ components/Todo.js
@@ -27,12 +27,22 @@ class Todo extends React.PureComponent {
   }
 
   render() {
+
+    // Trim the length of our todo item text if necessary
+    let todoText;
+    if (this.props.task.length >= 26) {
+      todoText = this.props.task.substr(0, 25) + "...";
+    } else {
+      todoText = this.props.task;
+    }
+
     return(
+
       <View style={{height: 100, flexDirection: 'row'}}>
 
         <LottieView style={{backgroundColor: 'white', height: 50, marginTop: 3, marginLeft: 10, alignSelf: 'center'}} source={require('../animations/attention.json')} loop={false} autoPlay/>
 
-        <Text style={{fontSize: 18, alignSelf: 'center', marginLeft: 25, color: "#404040"}}>{this.props.task}</Text>
+        <Text style={{fontSize: 15, alignSelf: 'center', marginLeft: 25, color: "#404040"}}>{todoText}</Text>
 
         <View style={{backgroundColor: 'white', alignSelf: 'stretch', flex: 1, flexDirection: 'row-reverse', alignItems: 'center', marginLeft: 30}}>
           <Checkbox 
