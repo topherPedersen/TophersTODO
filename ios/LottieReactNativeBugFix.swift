3454f3bc811c4633c7e997fe5a31fcb029502b04
Topher Pedersen <chris@topherpedersen.com>
2020-05-25 03:52:35 -0500

    got ios version up and running

---
+++ ios/LottieReactNativeBugFix.swift
@@ -0,0 +1,9 @@
+//
+//  LottieReactNativeBugFix.swift
+//  TophersTODO
+//
+//  Created by Christopher Pedersen on 5/25/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
